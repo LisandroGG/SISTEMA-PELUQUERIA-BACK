d4015e2dab6710b780b59d8d7b1df17236aadba5
Lisandro Pereyra <lisandro.pereyra123456@gmail.com>
2025-09-01 15:38:24 -0300

    test

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -17,8 +17,8 @@ const BASE_URL = API_STATUS === "production" ? DEPLOY : LOCALHOST;
 
 export const transporter = nodemailer.createTransport({
 	host: MAILER_HOST,
-	port: Number(MAILER_PORT),
-	secure: Number(MAILER_PORT) === 465,
+	port: MAILER_PORT,
+	secure: true,
 	auth: {
 		user: MAILER_USER,
 		pass: MAILER_PASSWORD,
